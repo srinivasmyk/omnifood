a3f2be14d6624c7396fa2b2cc6672fb691dc21b9
srinivasmyk <65235119+srinivasmyk@users.noreply.github.com>
2025-05-13 00:48:32 +0530

    Update global.js

--- global.js
+++ global.js
@@ -5,7 +5,7 @@
         (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
       var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
       var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-    })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-KBGIQSQYV18C-2");
+    })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-YBQEEASGFCZP-2");
      ///PX tag////
 
 // ----- DEMO USERS, RESTAURANTS, OFFERS -----
