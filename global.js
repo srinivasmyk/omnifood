546f8043d1bf6992480016f10b4ea946d5203c43
srinivasmyk <65235119+srinivasmyk@users.noreply.github.com>
2025-05-21 23:36:34 +0530

    Update global.js

--- global.js
+++ global.js
@@ -1,12 +1,14 @@
 
      ///PX tag////
+var config = {kcAllowedFuncNames : ["openHelpModal"]};
 
 (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
         (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
       var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
       var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-    })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-YBQEEASGFCZP-2");
-     ///PX tag////
+    })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-YBQEEASGFCZP-2",config);
+
+///PX tag////
 
 // ----- DEMO USERS, RESTAURANTS, OFFERS ---
 function getUSERS() {
